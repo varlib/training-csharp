c7f40adae64020a1e958082fa6fd6396f76189f9
Dmitry <l.dimka@gmail.com>
2021-02-16 10:12:47 +0300

    фильтрация слов в тексте

---
+++ Text/Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слова для удаления из текста");
+            string[] bad = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Введите текст");
+            string text = Console.ReadLine();
+
+            foreach (var item in bad)
+            {
+                text = text.Replace(item, new string('*', item.Length));
+            }
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
