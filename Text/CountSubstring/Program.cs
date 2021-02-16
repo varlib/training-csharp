6be2dc9f2afa661e60e97da37a43a1b2b35b0631
Dmitry <l.dimka@gmail.com>
2021-02-16 09:56:13 +0300

    Работа с текстом

---
+++ Text/CountSubstring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            string shablon = Console.ReadLine().ToLower();
+
+            int count = 0;
+            int index = text.IndexOf(shablon);
+
+            while (index != -1)
+            {
+                count++;
+                index = text.IndexOf(shablon, index + 1);
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Text/Palindromes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ', ',', '.', '!', '&' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> palindrom = new List<string>();
+            List<string> other = new List<string>();
+
+            foreach (var item in text)
+            {
+                if (item.SequenceEqual(item.Reverse()))
+                {
+                    palindrom.Add(item);
+                }
+                else
+                {
+                    other.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(", ", palindrom.OrderBy(x => x)));
+            Reverse(other);
+            Console.ReadLine();
+        }
+
+        static void Reverse(List<string> temp)
+        { 
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item.Reverse().ToArray());
+            }
+        }
+    }
+}
