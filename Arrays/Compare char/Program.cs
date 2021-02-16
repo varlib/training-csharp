bd0d3e52ca1500a5848f6bb6d1969ee2c5f2a326
Dmitry <l.dimka@gmail.com>
2021-02-16 10:25:14 +0300

    Update Program.cs

--- Arrays/Compare char/Program.cs
+++ Arrays/Compare char/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Compare_char
 {
@@ -6,7 +7,45 @@ namespace Compare_char
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Первая порция символов:");
+            char[] firstArr = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            Console.WriteLine("Вторая порция символов");
+            char[] secondArr = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            int len = Math.Min(firstArr.Length, secondArr.Length);
+            bool isFirst = false;
+
+            for(int i =0; i<len; i++)
+            {
+                if (firstArr[i] < secondArr[i])
+                {
+                    isFirst = true;
+                    break;
+                }else if (firstArr[i] > secondArr[i])
+                {
+                    isFirst = false;
+                    break;
+                }
+
+                if (i == len - 1)
+                {
+                    if (firstArr.Length < secondArr.Length)
+                    {
+                        isFirst = true;
+                    }
+                }
+            }
+            if (isFirst)
+            {
+                Console.WriteLine(string.Join("", firstArr));
+                Console.WriteLine(string.Join("", secondArr));
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", secondArr));
+                Console.WriteLine(string.Join("", firstArr));
+            }
+            Console.ReadKey();
         }
     }
 }
