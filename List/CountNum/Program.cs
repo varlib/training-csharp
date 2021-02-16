b0b05bdb2a9ad04ab3909e6e01b241567dfd1852
Dmitry <l.dimka@gmail.com>
2021-02-16 12:15:59 +0300

    Count num

---
+++ List/CountNum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через запятую");
+            List<int> num = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            num.Sort();
+
+            int firstNum = num[0];
+            int count = 1;
+
+            for (int i = 1; i < num.Count; i++)
+            {
+                if (firstNum == num[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNum} -> {count}");
+                    firstNum = num[i];
+                    count = 1;
+                }
+            }
+            Console.WriteLine($"{firstNum} -> {count}");
+            Console.ReadKey();
+        }
+    }
+}
