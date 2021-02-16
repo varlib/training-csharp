c34cfbc0fbff27fc2ded2558799c6d2efefd4927
Dmitry <l.dimka@gmail.com>
2021-02-16 11:02:45 +0300

    Append text

---
+++ Append/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Append
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слова через запятую");
+            List<string> numberText = Console.ReadLine().Split(",").Reverse().ToList();
+
+            foreach (var item in numberText)
+            {
+                string[] textArr = item.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(string.Join(" ", textArr) + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
