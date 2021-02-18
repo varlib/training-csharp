0be746e68347d267fda938f985fbf6cf78e0bd53
Dmitry <l.dimka@gmail.com>
2021-02-18 17:59:27 +0300

    sales

--- Classes/Sales/Program.cs
+++ Classes/Sales/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sales
 {
@@ -6,7 +7,41 @@ namespace Sales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Count sale:");
+            int countOfSale = int.Parse(Console.ReadLine());
+            Sale[] sales = new Sale[countOfSale];
+
+            for (int i = 0; i < countOfSale; i++)
+            {
+                Console.WriteLine("Write SALE (Town, Product, Price, Quantity)");
+                sales[i] = ReadSale(Console.ReadLine());
+            }
+
+            SortedDictionary<string, decimal> dictionary = new SortedDictionary<string, decimal>();
+
+            foreach (Sale sale in sales)
+            {
+                if (dictionary.ContainsKey(sale.Town))
+                {
+                    dictionary[sale.Town] += sale.Price * sale.Quantity;
+                }
+                else
+                {
+                    dictionary[sale.Town] = sale.Price * sale.Quantity;
+                }
+            }
+
+            foreach (var pair in dictionary)
+            {
+                Console.WriteLine($"Key: {pair.Key} | Value: {pair.Value}");
+            }
+            Console.ReadKey();
+        }
+
+        static Sale ReadSale(string input)
+        {
+            string[] token = input.Split(' ');
+            return new Sale { Town = token[0], Product = token[1], Price = decimal.Parse(token[2]), Quantity = decimal.Parse(token[3]) };
         }
     }
 }
---
+++ Classes/Sales/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    class Sale
+    {
+        public string Town { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+
+    }
+}
