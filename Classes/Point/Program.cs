756eda934cd40e68ef5c9be74bedc8c85471be82
Dmitry <l.dimka@gmail.com>
2021-02-18 15:12:49 +0300

    Classes

---
+++ Classes/Point/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
