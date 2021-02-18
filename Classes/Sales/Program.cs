795c5f8b72442fe8f92ba68cf888682a5024322d
Dmitry <l.dimka@gmail.com>
2021-02-18 15:31:50 +0300

    Sales

---
+++ Classes/Sales/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
